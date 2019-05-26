36aaf75e15d643cb81dc555f71825fc987a5b479
Zhichang Yu <yuzhichang@gmail.com>
2019-05-26 11:46:57 +0800

    updated images

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --tag infinivision/stolon-keeper --target keeper .
-docker build --tag infinivision/stolon-proxy --target proxy .
-docker build --tag infinivision/stolon-sentinel --target sentinel .
+docker build --tag infinivision/stolon-keeper --target keeper . \
+&& docker build --tag infinivision/stolon-proxy --target proxy . \
+&& docker build --tag infinivision/stolon-sentinel --target sentinel .
