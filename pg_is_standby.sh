59ed6907178beed38c5b7a8ca78984adeb8e47fc
Zhichang Yu <yuzhichang@gmail.com>
2019-06-25 17:07:15 +0800

    revised pg_is_master.sh

--- pg_is_standby.sh
+++ pg_is_standby.sh
@@ -4,6 +4,7 @@
 # prepare pgpass:
 # echo "*:*:*:postgres:supassword" > pgpass && chmod 600 pgpass && sudo chown haproxy pgpass && sudo mv pgpass /etc/haproxy/pgpass
 
-echo "pg_is_standby $HAPROXY_SERVER_ADDR:$HAPROXY_SERVER_PORT"
-PGPASSFILE=/etc/haproxy/pgpass psql --host "$HAPROXY_SERVER_ADDR" --port "$HAPROXY_SERVER_PORT" --username=postgres --no-password --pset=tuples_only -c "SELECT pg_is_in_recovery();" | grep -P "^\s*t\s*$"
-exit $?
+PGPASSFILE=/etc/haproxy/pgpass psql --host "$HAPROXY_SERVER_ADDR" --port "$HAPROXY_SERVER_PORT" --username=postgres --no-password --pset=tuples_only -c "SELECT pg_is_in_recovery();" | grep -P "^\s*t\s*$" > /dev/null
+RET=$?
+echo `date --rfc-3339=seconds` "$HAPROXY_SERVER_ADDR:$HAPROXY_SERVER_PORT pg_is_standby RET $RET"
+exit "$RET"
