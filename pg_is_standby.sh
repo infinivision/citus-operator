81f3b07724ecd828f353936c3ad80fb409649290
Zhichang Yu <yuzhichang@gmail.com>
2019-07-15 17:52:01 +0800

    improved pg check script performance

--- pg_is_standby.sh
+++ pg_is_standby.sh
@@ -4,7 +4,13 @@
 # prepare pgpass:
 # echo "*:*:*:postgres:supassword" > pgpass && chmod 600 pgpass && sudo chown haproxy pgpass && sudo mv pgpass /etc/haproxy/pgpass
 
-PGPASSFILE=/etc/haproxy/pgpass psql "host=$HAPROXY_SERVER_ADDR port=$HAPROXY_SERVER_PORT connect_timeout=1" --username=postgres --no-password --pset=tuples_only -c "SELECT pg_is_in_recovery();" | grep -P "^\s*t\s*$" > /dev/null
+# psql checking is heavy, each run costs 300ms~1s.
+#PGPASSFILE=/etc/haproxy/pgpass psql "host=$HAPROXY_SERVER_ADDR port=$HAPROXY_SERVER_PORT connect_timeout=1" --username=postgres --no-password --pset=tuples_only -c "SELECT pg_is_in_recovery();" | grep -P "^\s*t\s*$" > /dev/null
+
+# Assumes keeper metric port is postgres port plus 1.
+let STKEEPER_METRICS_PORT=$HAPROXY_SERVER_PORT+1
+curl http://$HAPROXY_SERVER_ADDR:$STKEEPER_METRICS_PORT/metrics 2>/dev/null | grep -P "^stolon_keeper_local_role.*standby.*1$" > /dev/null
+
 RET=$?
 #echo `date --rfc-3339=seconds` "$HAPROXY_SERVER_ADDR:$HAPROXY_SERVER_PORT pg_is_standby RET $RET"
 exit "$RET"
